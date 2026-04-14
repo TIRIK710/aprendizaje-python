22a6f45be6cfef735877574d16ae49d70b94c112
TIRIK710 <mmedinac2003@gmail.com>
2026-04-13 22:55:29 -0400

    Actualizar conversor-temperatura.py

--- mini proyectos/conversor-temperatura.py
+++ mini proyectos/conversor-temperatura.py
@@ -6,9 +6,11 @@ while True:
     
     if num2 == "fahrenheit":
         resultado = num1 - 32 / 1.8
+        print(f"{num1} grados celcius equivalen a {resultado} grados fahrenheit")
         break
     elif num2 == "celcius":
         resultado = num1 * 1.8 + 32
+        print(f"{num1} grados fahrenheit equivalen a {resultado} grados celcius")
         break
     else:
         print("eso no tiene sentido")
