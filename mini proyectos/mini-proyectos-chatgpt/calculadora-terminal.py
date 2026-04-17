2e8c191badf1499eac3f55fc5b4f56e404b097de
TIRIK710 <mmedinac2003@gmail.com>
2026-04-17 02:24:15 -0400

    Refactor calculator logic to improve input handling and remove eval usage

--- mini proyectos/mini-proyectos-chatgpt/calculadora-terminal.py
+++ mini proyectos/mini-proyectos-chatgpt/calculadora-terminal.py
@@ -4,16 +4,45 @@
 3. nos pida una operacion (+ - * /)
 """
 
+# V1
+
+# lista_operation = ["+", "-", "*", "/"]
+
+# num1 = float(input("ingrese el primer numero: "))
+# num2 = float(input("ingrese el segundo numero: "))
+# operation = input(f"ingresa una operacion {' '.join(lista_operation)}: ")
+
+# while operation not in lista_operation:
+#    operation = input("ingresa una operacion (+ - * /): ")
+
+# try:
+#    print(eval(f"{num1} {operation} {num2}"))
+# except ZeroDivisionError:
+#    print("no se puede dividir por")
+
+################################################################################
+
+# V2
+
 lista_operation = ["+", "-", "*", "/"]
 
 num1 = float(input("ingrese el primer numero: "))
-num2 = float(input("ingrese el segundo numero: "))
 operation = input(f"ingresa una operacion {' '.join(lista_operation)}: ")
 
 while operation not in lista_operation:
-    operation = input("ingresa una operacion (+ - * /): ")
+    operation = input(f"ingresa una operacion: {' '.join(lista_operation)}: ")
 
-try:
-    print(eval(f"{num1} {operation} {num2}"))
-except ZeroDivisionError:
-    print("no se puede dividir por")
+num2 = float(input("ingrese el segundo numero: "))
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+else:
+    if num2 == 0:
+        quit("sin definir")
+    result = num1 / num2
+
+print(result)
