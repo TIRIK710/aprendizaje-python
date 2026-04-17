1bbcd77392c143909196b02e436f3e4bd41a7d62
TIRIK710 <mmedinac2003@gmail.com>
2026-04-17 02:25:15 -0400

    Refactor input prompt in rondas function for better readability

--- mini proyectos/piedra-papel-tijeras-2.0.py
+++ mini proyectos/piedra-papel-tijeras-2.0.py
@@ -34,9 +34,10 @@ def jugar():
 
 def rondas():
     lista_rondas = ["1", "3", "5", "7"]
-    """Solicita al usuario que elija el número de rondas a jugar (1, 3 o 5)."""
+    # Solicita al usuario que elija el número de rondas a jugar (1, 3 o 5).
     while True:
-        elejir = input(f"Cuantas rondas quieres jugar?: {', '.join(lista_rondas)}")
+        elejir = input(
+            f"Cuantas rondas quieres jugar?: {', '.join(lista_rondas)}")
         if elejir not in lista_rondas:
             print("Intenta otra vez")
             continue
