b12d6c192acd060e7eb28f770ab8764b40268f77
Tirik710 <mmedina2003@gmail.com>
2026-05-25 21:53:22 -0400

    Refactor menu function to encapsulate calculator logic and improve structure

--- mini proyectos/mini-proyectos-chatgpt/calculadora-funciones.py
+++ mini proyectos/mini-proyectos-chatgpt/calculadora-funciones.py
@@ -18,44 +18,49 @@ def dividir(a, b):
         return "Indeterminado"
     return a / b
 
-# menu
+def menu():
+    while True:
+        
+        print("Bienvenido a la app Calculadora")
+
+        try:
+            num1 = float(input("ingrese el primer numero: "))
+        except ValueError:
+            print("Caracter invalido")
+            continue
+        
+        operacion = input(f"Ingrese la operacion que desea realizar: {", ".join(operaciones)}: ")
+        if operacion not in operaciones:
+            print("la operacion ingresada no es valida")
+            continue
+        
+        try:
+            num2 = float(input("ingrese el segundo numero: "))
+        except ValueError:
+            print("Caracter invalido")
+            continue
+        
+        if operacion == "+":
+            print(f"el resultado es: {sumar(num1, num2)}")
+        elif operacion == "-":
+            print(f"el resultado es: {restar(num1, num2)}")
+        elif operacion == "*":
+            print(f"el resultado es: {multiplicar(num1, num2)}")
+        elif operacion == "/":
+            print(f"el resultado es: {dividir(num1, num2)}")
+        break
+
+menu()
+
+# close app
 while True:
-    
-    print("Bienvenido a la app Calculadora")
-
-    try:
-        num1 = float(input("ingrese el primer numero: "))
-    except ValueError:
-        print("Caracter invalido")
-        continue
-    
-    operacion = input(f"Ingrese la operacion que desea realizar: {", ".join(operaciones)}: ")
-    if operacion not in operaciones:
-        print("la operacion ingresada no es valida")
-        continue
-    
-    try:
-        num2 = float(input("ingrese el segundo numero: "))
-    except ValueError:
-        print("Caracter invalido")
-        continue
-      
-    if operacion == "+":
-        print(f"el resultado es: {sumar(num1, num2)}")
-    elif operacion == "-":
-        print(f"el resultado es: {restar(num1, num2)}")
-    elif operacion == "*":
-        print(f"el resultado es: {multiplicar(num1, num2)}")
-    elif operacion == "/":
-        print(f"el resultado es: {dividir(num1, num2)}")
-
-    # close app
     close = input("Desea volver al inicio? Y/N: ").lower()
 
     if close == "y":
-        continue
+        menu()
     elif close == "n":
         print("Cerrando...")
         break
     else:
         print("Accion no reconocida. Intentalo denuevo")
+        continue
